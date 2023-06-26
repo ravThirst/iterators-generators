d94f7ae0673d245f2dc4fe3b10688206396234e6
ravThirst <99572822+ravThirst@users.noreply.github.com>
2023-06-27 01:57:06 +1000

    added scripts

---
+++ main.py
@@ -0,0 +1,75 @@
+import types
+
+
+class FlatIterator:
+    def __init__(self, list_of_list):
+        self.flat_list = []
+        self.set_flat(list_of_list)
+        self.counter = -1
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        self.counter += 1
+        if self.counter >= len(self.flat_list):
+            raise StopIteration
+        return self.flat_list[self.counter]
+
+    def set_flat(self, list_of_lists):
+        for pos, element in enumerate(list_of_lists):
+            if type(element) is list:
+                self.set_flat(list_of_lists[pos])
+            else:
+                self.flat_list.append(element)
+
+
+def flat_generator(list_of_lists):
+    for item in list_of_lists:
+        if type(item) is list:
+            yield from flat_generator(item)
+        else:
+            yield item
+
+
+def test_1():
+
+    list_of_lists_1 = [
+        ['a', 'b', 'c'],
+        ['d', 'e', 'f', 'h', False],
+        [1, 2, None]
+    ]
+
+    for flat_iterator_item, check_item in zip(
+            FlatIterator(list_of_lists_1),
+            ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+    ):
+
+        assert flat_iterator_item == check_item
+
+    assert list(FlatIterator(list_of_lists_1)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+
+
+def test_2():
+
+    list_of_lists_1 = [
+        ['a', 'b', 'c'],
+        ['d', 'e', 'f', 'h', False],
+        [1, 2, None]
+    ]
+
+    for flat_iterator_item, check_item in zip(
+            flat_generator(list_of_lists_1),
+            ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+    ):
+
+        assert flat_iterator_item == check_item
+
+    assert list(flat_generator(list_of_lists_1)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+
+    assert isinstance(flat_generator(list_of_lists_1), types.GeneratorType)
+
+
+if __name__ == '__main__':
+    test_1()
+    test_2()
